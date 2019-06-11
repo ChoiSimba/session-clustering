51246df17bce52af934fa77694935ed561927a73
ChoiSimba <antiair88@gmail.com>
2019-06-12 00:16:47 +0900

    Modify TestObjectMapper class.

--- src/test/java/io/simba/session/ObjectMapperTests.java
+++ src/test/java/io/simba/session/ObjectMapperTests.java
@@ -4,19 +4,21 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.simba.session.dto.User;
 import org.junit.Test;
+import org.springframework.util.Assert;
 
-public class TestObjectMapper {
+
+public class ObjectMapperTests {
     @Test
-    public void test_objectMapper() throws Exception {
+    public void addClassTypeInJson() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper()
                 .enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
 
         byte[] serializedBytes = objectMapper.writeValueAsBytes(new User("antiair88", "simba", "antiair88@gmail.com"));
-
         System.out.println("serialized : " + new String(serializedBytes));
 
         Object deserializedObject = objectMapper.readValue(serializedBytes, Object.class);
-
         System.out.println("deserialized : " + deserializedObject.toString());
+
+        Assert.isInstanceOf(User.class, deserializedObject);
     }
 }
